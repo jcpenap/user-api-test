5775903c0244ce09722abcfa87949465e77e37a2
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-06-05 20:11:50 -0500

    Se agregan cambios con funcion de sentry

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv').config();
 
 const api = require('./api');
 const config = require('./config');
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://ccb2bbeae94644a6b7d72a8c1a7e76c6@o403576.ingest.sentry.io/5266429' });
 
 const app = express();
 app.use(express.json());
